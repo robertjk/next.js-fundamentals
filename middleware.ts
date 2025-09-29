21231128c9547abbc6e06b606919242324e3bb9c
Robert J. Kusznier <no-cheating@autistici.org>
2025-09-29 19:43:06 +0200

    Add authentication middleware

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/api")) {
+    const authHeader = (await headers()).get("Authorization");
+
+    if (!authHeader) {
+      return NextResponse.json(
+        { success: false, message: "Authorization header is required" },
+        { status: 401 }
+      );
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
